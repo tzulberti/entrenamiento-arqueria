46d2efe36c5f3c49ba34119dbb08a560693414b6
Tomas Zulberti <tzulberti@gmail.com>
2013-11-04 22:13:21 -0300

    Se agregaron algunos archivos basicos que faltaban

---
+++ setup.py
@@ -0,0 +1,29 @@
+from setuptools import setup, find_packages
+
+
+setup(
+    name='entrenamiento-arqueria',
+    version='0.0.1',
+    description='Un sistema para manejar el enternamiento de arqueria',
+    long_description=open('README.rst').read(-1),
+    author='Tomas Zulberti',
+    author_email='tzulberti@gmail.com',
+    license='MIT',
+    url='https://github.com/tzulberti/entrenamiento-arqueria',
+    install_requires=[
+        "Flask",
+        "requests",
+    ],
+    packages=find_packages(),
+    include_package_data=True,
+    zip_safe=False,
+    keywords='pypi flask proxy',
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Environment :: Web Environment',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python :: 2.6',
+        'Programming Language :: Python :: 2.7',
+    ]
+)
