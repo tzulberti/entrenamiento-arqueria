1111a8ca1e4c678ba171323e6b361ce0b7c7e590
Tomas Zulberti <tzulberti@gmail.com>
2014-03-13 07:30:51 -0300

    Solved bug
    Se redirige al usuario si el mismo no esta logueado.

--- entrenamiento/views/index.py
+++ entrenamiento/views/index.py
@@ -1,13 +1,16 @@
 # -*- coding: utf-8 -*-
 
-from flask import render_template
+from flask import render_template, session, redirect, url_for
+from flask.views import MethodView
 
-from entrenamiento.views.base import BaseEntrenamientoView
-
-class IndexViewTemplate(BaseEntrenamientoView):
+class IndexViewTemplate(MethodView):
     ''' Se encarga de renderar el index una vez que el usuario ya se pudo
     loguear correctamente.
     '''
 
     def get(self):
-        return render_template('index.html')
+        if 'logged_user' in session:
+            return render_template('index.html')
+        else:
+            return redirect(url_for('auth.login'))
+
