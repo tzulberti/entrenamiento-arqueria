b06354fb2d5279e92ddd67281108782c1d85f9c6
Tomas Zulberti <tzulberti@gmail.com>
2014-04-29 07:17:41 -0300

    Se saco el template del javascript

--- entrenamiento/static/js/crud/invitacion.js
+++ entrenamiento/static/js/crud/invitacion.js
@@ -5,21 +5,6 @@
 var InvitacionCrudApplication = BaseTemplateCrudApp.$extend({
 
     __init__: function(historyManager, element) {
-        var formTemplate = '' +
-            '<form role="form" class="form-horizontal">' +
-                '<div class="form-group">' +
-                    '<label for="nombre" class="col-sm-2 control-label">Email</label>' +
-                    '<div class="col-sm-10">' +
-                        '<input type="text" name="email" id="email" class="form-control">' +
-                        '<span class="help-block">El email de la persona a quien se le tiene que enviar la invitacion para que se cree un usuario</span>' +
-                    '</div>' +
-                '</div>' +
-                '<div class="form-group">' +
-                    '<div class="col-sm-10 col-sm-offset-2">' +
-                        '<button type="submit" class="btn btn-primary button-save">Grabar</button>' +
-                    '</div>' +
-                '</div>' +
-            '</form>';
         var columnNames = [
             'email',
             'usada',
