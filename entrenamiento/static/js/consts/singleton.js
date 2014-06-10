d87d27bf8560cc529b83a5e3a893b6e204fbd432
Tomas Zulberti <tzulberti@gmail.com>
2014-06-10 06:34:30 -0300

    Ya no se lo usaba mas

--- entrenamiento/static/js/consts/singleton.js
+++
@@ -1,8 +0,0 @@
-// Tiene todas las instancias de las cosas que estan en consts
-// para no crear mucha de las mismas.
-var consts = {
-    tipoTorneos: new TipoTorneos(),
-
-    // indica el path basico para todos los llamados ajax correspondientes.
-    BASE_API_URL: '/api/v01/'
-};
