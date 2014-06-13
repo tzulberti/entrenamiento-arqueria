ac8b7edf03d67082e90b923c403f9629f20ff96f
Tomas Zulberti <tzulberti@gmail.com>
2014-06-13 20:08:38 -0300

    Se borro un metodo que no se usaba

--- entrenamiento/static/js/utils/utils.js
+++ entrenamiento/static/js/utils/utils.js
@@ -1,25 +1,3 @@
-/**
- * Equivalente a la funcion range de python.
- */
-function range(start, stop, step){
-    if (typeof stop=='undefined'){
-        // one param defined
-        stop = start;
-        start = 0;
-    };
-    if (typeof step=='undefined'){
-        step = 1;
-    };
-    if ((step>0 && start>=stop) || (step<0 && start<=stop)){
-        return [];
-    };
-    var result = [];
-    for (var i=start; step>0 ? i<stop : i>stop; i+=step){
-        result.push(i);
-    };
-    return result;
-};
-
 var utils = {
 
 
