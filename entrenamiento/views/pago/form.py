8e811c28539e77245d1bb5f3e56c8166b1a2aa27
Tomas Zulberti <tzulberti@gmail.com>
2014-06-13 07:38:29 -0300

    Solved bug
    Se hizo que el mes correspodiente sea el primer dia de
    cada mes

--- entrenamiento/views/pago/form.py
+++ entrenamiento/views/pago/form.py
@@ -1,5 +1,7 @@
 # -*- coding: utf-8 -*-
 
+from datetime import date
+
 from entrenamiento.views.form import ModelForm
 from entrenamiento.views.utils import get_logged_user_data
 
@@ -19,5 +21,8 @@ class PagoForm(ModelForm):
         res = super(PagoForm, self).get_instance()
         logged_user = get_logged_user_data()
         res.id_cargado_por = logged_user.id
+        res.mes_correspondiente = date(res.mes_correspondiente.year,
+                                       res.mes_correspondiente.month,
+                                       1)
         return res
 
