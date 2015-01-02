ee43a34597f0d2cb3c427170e79461fc7e44be81
Tomas Zulberti <tzulberti@gmail.com>
2015-01-02 09:26:47 -0300

    Improved how models where loaded

--- entrenamiento/app/singleton.py
+++ entrenamiento/app/singleton.py
@@ -6,25 +6,13 @@ applicacion.
 Esto es para evitar crear mas de una misma instancia de la clase
 '''
 
+import importlib
+import os
+import pkgutil
 from entrenamiento.app.app import app
+from entrenamiento import models
+from entrenamiento.models.base import BaseConstModel, BaseModel
 from entrenamiento.models import consts_tables
-from entrenamiento.models.arquero import Arquero
-from entrenamiento.models.asistencia import Asistencia
-from entrenamiento.models.base import BaseConstModel
-from entrenamiento.models.flechas import Flechas
-from entrenamiento.models.gasto import Gasto
-from entrenamiento.models.invitacion import Invitacion
-from entrenamiento.models.lugar import Lugar
-from entrenamiento.models.usuario import Usuario
-from entrenamiento.models.arco import Arco, ArcoRecurvado
-from entrenamiento.models.pago import Pago
-from entrenamiento.models.torneo import Torneo, Ronda, Serie
-from entrenamiento.models.turno import Turno
-from entrenamiento.models.historia_estado_arquero import HistoriaEstadoArquero
-from entrenamiento.models.entrenamiento_realizado import (EntrenamientoRealizado,
-                                                          EntrenamientoFlechas)
-from entrenamiento.models.permiso import Permiso, PermisoUsuario
-
 from entrenamiento.utils import MailSender, DatabaseInformation
 
 #: la instancia que se va a usar para enviarle mails a los diferentes usuarios.
@@ -32,28 +20,23 @@ mail_sender = MailSender(app.config['EMAIL_USERNAME'],
                          app.config['EMAIL_PASSWORD'])
 
 
-models = [
-    Arquero,
-    Asistencia,
-    Invitacion,
-    Lugar,
-    Usuario,
-    Arco,
-    ArcoRecurvado,
-    Pago,
-    Torneo,
-    Turno,
-    Ronda,
-    Serie,
-    HistoriaEstadoArquero,
-    EntrenamientoRealizado,
-    EntrenamientoFlechas,
-    Gasto,
-    Flechas,
-    Permiso,
-    PermisoUsuario,
-]
-
+models_klass = set()
+models_path = os.path.dirname(models.__file__)
+models_modules_names = [name for _, name, _ in pkgutil.iter_modules([models_path])]
+models_modules_names.remove('base')
+models_modules_names.remove('consts_tables')
+for module_name in models_modules_names:
+    module = importlib.import_module('entrenamiento.models.%s' % module_name)
+    for attr_name in dir(module):
+        attr = getattr(module, attr_name)
+        if not hasattr(attr, 'mro'):
+            continue
+        if attr == BaseConstModel:
+            continue
+        if attr == BaseModel:
+            continue
+        if BaseModel in attr.mro():
+            models_klass.add(attr)
 
 
 const_tables = []
@@ -68,6 +51,9 @@ for attr_name in dir(consts_tables):
 
 #: la instancia que tiene toda la informacion sobre el schema de la base de
 #: datos.
-database_information = DatabaseInformation(models,
+database_information = DatabaseInformation(list(models_klass),
                                            const_tables)
 database_information.get()
+
+
+
